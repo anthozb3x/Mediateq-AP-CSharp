44d766bf238641c160331426f45611843c3af72f
anthozb3x <anthony.beal@clsi.fr>
2022-11-08 15:10:58 +0100

    Ajoutez des fichiers projet.

---
+++ FrmMediateq.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mediateq_AP_SIO2.metier;
+
+
+namespace Mediateq_AP_SIO2
+{
+    public partial class FrmMediateq : Form
+    {
+        #region Variables globales
+
+        static List<Categorie> lesCategories;
+        static List<Descripteur> lesDescripteurs;
+        static List<Revue> lesTitres;
+        static List<Livre> lesLivres;
+
+        #endregion
+
+
+        #region Procédures évènementielles
+
+        public FrmMediateq()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMediateq_Load(object sender, EventArgs e)
+        {
+            // Création de la connexion avec la base de données
+            DAOFactory.creerConnection();
+
+            // Chargement des objets en mémoire
+            lesDescripteurs = DAODocuments.getAllDescripteurs();
+            lesTitres = DAOPresse.getAllTitre();
+
+        }
+
+        #endregion
+
+
+        #region Parutions
+        //-----------------------------------------------------------
+        // ONGLET "PARUTIONS"
+        //------------------------------------------------------------
+        private void tabParutions_Enter(object sender, EventArgs e)
+        {
+            cbxTitres.DataSource = lesTitres;
+            cbxTitres.DisplayMember = "titre";
+        }
+
+        private void cbxTitres_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                List<Parution> lesParutions;
+
+                Revue titreSelectionne = (Revue)cbxTitres.SelectedItem;
+                lesParutions = DAOPresse.getParutionByTitre(titreSelectionne);
+
+                // ré-initialisation du dataGridView
+                dgvParutions.Rows.Clear();
+
+                // Parcours de la collection des titres et alimentation du datagridview
+                foreach (Parution parution in lesParutions)
+                {
+                    dgvParutions.Rows.Add(parution.Numero, parution.DateParution, parution.Photo);
+                }
+            
+        }
+        #endregion
+
+
+        #region Revues
+        //-----------------------------------------------------------
+        // ONGLET "TITRES"
+        //------------------------------------------------------------
+        private void tabTitres_Enter(object sender, EventArgs e)
+        {
+            cbxDomaines.DataSource = lesDescripteurs;
+            cbxDomaines.DisplayMember = "libelle";
+        }
+
+        private void cbxDomaines_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Objet Domaine sélectionné dans la comboBox
+            Descripteur domaineSelectionne = (Descripteur)cbxDomaines.SelectedItem;
+
+            // ré-initialisation du dataGridView
+            dgvTitres.Rows.Clear();
+
+            // Parcours de la collection des titres et alimentation du datagridview
+            foreach (Revue revue in lesTitres)
+            {
+                if (revue.IdDescripteur==domaineSelectionne.Id)
+                {
+                    dgvTitres.Rows.Add(revue.Id, revue.Titre, revue.Empruntable, revue.DateFinAbonnement, revue.DelaiMiseADispo);
+                }
+            }
+        }
+        #endregion
+
+
+        #region Livres
+        //-----------------------------------------------------------
+        // ONGLET "LIVRES"
+        //-----------------------------------------------------------
+
+        private void tabLivres_Enter(object sender, EventArgs e)
+        {
+            // Chargement des objets en mémoire
+            lesCategories = DAODocuments.getAllCategories();
+            lesDescripteurs = DAODocuments.getAllDescripteurs();
+            lesLivres = DAODocuments.getAllLivres();
+            //DAODocuments.setDescripteurs(lesLivres);
+        }
+   
+        private void btnRechercher_Click(object sender, EventArgs e)
+        {
+            // On réinitialise les labels
+            lblNumero.Text = "";
+            lblTitre.Text = "";
+            lblAuteur.Text = "";
+            lblCollection.Text = "";
+            lblISBN.Text = "";
+            lblImage.Text = "";
+            lblCategorie.Text = "";
+
+            // On recherche le livre correspondant au numéro de document saisi.
+            // S'il n'existe pas: on affiche un popup message d'erreur
+            bool trouve = false;
+            foreach (Livre livre in lesLivres)
+            {
+                if (livre.IdDoc==txbNumDoc.Text)
+                {
+                    lblNumero.Text = livre.IdDoc;
+                    lblTitre.Text = livre.Titre;
+                    lblAuteur.Text = livre.Auteur;
+                    lblCollection.Text = livre.LaCollection;
+                    lblISBN.Text = livre.ISBN1;
+                    lblImage.Text = livre.Image;
+                    lblCategorie.Text = livre.LaCategorie.Libelle;
+                    trouve = true;
+                }
+            }
+            if (!trouve)
+                MessageBox.Show("Document non trouvé dans les livres");
+        }
+
+        private void txbTitre_TextChanged(object sender, EventArgs e)
+        {
+            dgvLivres.Rows.Clear();
+
+            // On parcourt tous les livres. Si le titre matche avec la saisie, on l'affiche dans le datagrid.
+            foreach (Livre livre in lesLivres)
+            {
+                // on passe le champ de saisie et le titre en minuscules car la méthode Contains
+                // tient compte de la casse.
+                string saisieMinuscules;
+                saisieMinuscules = txbTitre.Text.ToLower();
+                string titreMinuscules;
+                titreMinuscules = livre.Titre.ToLower();
+
+                //on teste si le titre du livre contient ce qui a été saisi
+                if (titreMinuscules.Contains(saisieMinuscules))
+                {
+                    dgvLivres.Rows.Add(livre.IdDoc,livre.Titre,livre.Auteur,livre.ISBN1,livre.LaCollection);
+                }
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ modele/DAODocuments.cs
@@ -0,0 +1,119 @@
+﻿using Mediateq_AP_SIO2.metier;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediateq_AP_SIO2
+{
+    class DAODocuments
+    {
+        public static List<Categorie> getAllCategories()
+        {
+            List<Categorie> lesCategories = new List<Categorie>();
+            string req = "Select * from categorie";
+
+            DAOFactory.connecter();
+
+            MySqlDataReader reader = DAOFactory.execSQLRead(req);
+
+            while (reader.Read())
+            {
+                Categorie categorie = new Categorie(reader[0].ToString(), reader[1].ToString());
+                lesCategories.Add(categorie);
+            }
+            DAOFactory.deconnecter();
+            return lesCategories;
+        }
+
+        public static List<Descripteur> getAllDescripteurs()
+        {
+            List<Descripteur> lesDescripteurs = new List<Descripteur>();
+            string req = "Select * from descripteur";
+
+            DAOFactory.connecter();
+
+            MySqlDataReader reader = DAOFactory.execSQLRead(req);
+
+            while (reader.Read())
+            {
+                Descripteur genre = new Descripteur(reader[0].ToString(), reader[1].ToString());
+                lesDescripteurs.Add(genre);
+            }
+            DAOFactory.deconnecter();
+            return lesDescripteurs;
+        }
+        
+        public static List<Livre> getAllLivres()
+        {
+            List<Livre> lesLivres = new List<Livre>();
+            string req = "Select l.id, l.ISBN, l.auteur, d.titre, d.image, l.collection, d.idPublic, c.libelle from livre l ";
+            req += " join document d on l.id=d.id";
+            req += " join categorie c on d.idPublic = c.id";
+
+            DAOFactory.connecter();
+
+            MySqlDataReader reader = DAOFactory.execSQLRead(req);
+
+            while (reader.Read())
+            {
+                Livre livre = new Livre(reader[0].ToString(), reader[3].ToString(), reader[1].ToString(),
+                reader[2].ToString(), reader[5].ToString(), reader[4].ToString(),new Categorie(reader[6].ToString(),reader[7].ToString()));
+  
+                lesLivres.Add(livre);
+                
+            }
+
+            DAOFactory.deconnecter();
+
+            return lesLivres;
+        }
+
+        public static void setDescripteurs(List<Livre> lesLivres)
+        {
+            DAOFactory.connecter();
+
+            foreach (Livre livre in lesLivres)
+            {
+                List<Descripteur> lesDescripteursDuLivre = new List<Descripteur>(); ;
+                string req = "Select de.id, de.libelle from descripteur de ";
+                req += " join est_decrit_par e on de.id = e.idDesc";
+                req += " join document do on do.id = '" + livre.IdDoc + "'";
+                             
+                MySqlDataReader reader = DAOFactory.execSQLRead(req);
+                while (reader.Read())
+                {
+                    lesDescripteursDuLivre.Add(new Descripteur(reader[0].ToString(), reader[1].ToString()));
+                }
+                livre.LesDescripteurs = lesDescripteursDuLivre;
+            }
+            DAOFactory.deconnecter();
+        }
+
+        public static Categorie getCategorieByLivre(Livre pLivre)
+        {
+            Categorie categorie;
+            string req = "Select c.id,c.libelle from categorie c,document d where c.id = d.idPublic and d.id='";
+            req += pLivre.IdDoc + "'";
+
+            DAOFactory.connecter();
+
+            MySqlDataReader reader = DAOFactory.execSQLRead(req);
+
+            if (reader.Read())
+            {
+                categorie = new Categorie(reader[0].ToString(), reader[1].ToString());
+            }
+            else
+            {
+                categorie = null;
+            }
+            DAOFactory.deconnecter();
+            return categorie;
+        }
+
+    }
+
+}
